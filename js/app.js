e6ab7a4cc2c52c8971a2c0bcecca03a7bd18dcce
Jose Niquen <j.armando0807@gmail.com>
2021-03-28 16:30:43 -0500

    mejoras...

--- js/app.js
+++ js/app.js
@@ -93,8 +93,8 @@ popUpMenu();
 
 simplyCountdown('#cuenta', {
     year: 2021, // required
-    month: 3, // required
-    day: 5, // required
+    month: 4, // required
+    day: 23, // required
     hours: 17, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
